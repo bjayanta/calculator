f366429b3c3c3ed4efbc741e63c19c70a93322b0
Jayanta <bjayanta.me@gmail.com>
2025-07-03 16:42:47 +0600

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import Calculator from "@/components/Calculator";
+import { Stack } from "expo-router";
+
+export default function Index() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Calculator" }} />
+      <Calculator />
+    </>
+  );
+}
---
+++ components/Button.tsx
@@ -0,0 +1,49 @@
+import { Colors } from "@/utils/Colors";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+type ButtonProps = {
+  title: string;
+  type: "top" | "right" | "number";
+  onPress: Function;
+};
+
+export default function Button({ title, type, onPress }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        {
+          backgroundColor:
+            type === "top"
+              ? Colors.btnDark
+              : type === "right"
+              ? Colors.btnRight
+              : Colors.btnLight,
+        },
+      ]}
+      onPress={onPress}
+    >
+      <Text
+        style={{
+          fontSize: 34,
+          color: type === "number" ? Colors.black : Colors.white,
+        }}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    height: 70,
+    width: 70,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.btnDark,
+  },
+});
